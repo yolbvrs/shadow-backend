bd20c8fcac6afd1adf80fedc34e792e3ecd3b037
yolbvrs <myhreyolbars@gmail.com>
2025-03-05 17:56:49 +0800

    Update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config'); // ✅ MongoDB connection
 require('dotenv').config();
 
 const app = express();
